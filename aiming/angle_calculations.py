ac644d685fb0b1a401206215814077d9323c6a05
Anuj Pannala <anujpannala@gmail.com>
2025-04-04 16:36:55 -0400

    Added calculation tests

--- aiming/angle_calculations.py
+++ aiming/angle_calculations.py
@@ -1,7 +1,7 @@
-import math;
+import math
 
 def launch_velocity(hoop_distance, angle):
-    ramp_height = 1 #This is unknown
+    ramp_height = 0 #This is unknown
     g = 9.81
     hoop_height = 3.05
     #hoop_distance measured by LiDAR
@@ -11,6 +11,9 @@ def launch_velocity(hoop_distance, angle):
 
     num_1 = g * (hoop_distance ** 2)
     num_2 = 2 * (hoop_distance * math.tan(theta) - (hoop_height - ramp_height)) * (math.cos(theta) ** 2)
+
+    if ((num_1 / num_2) < 0):
+        return 1000
 
     return math.sqrt(num_1 / num_2)
 
@@ -24,4 +27,9 @@ def ramp_angle(hoop_distance):
             v0 = j
             theta = i
 
-    return v0
+    print(theta)
+    print(v0)
+
+
+ramp_angle(7.239) #3 point shot
+ramp_angle(4.572) #Free throw line
