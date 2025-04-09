d0494375af0db0640d38e71717e36ba4b301320b
Anuj Pannala <anujpannala@gmail.com>
2025-04-09 15:52:27 -0400

    Added mecanum wheel functions

---
+++ aiming/wheels.py
@@ -0,0 +1,44 @@
+import Math
+
+#Method 1
+#Takes input from a controller
+#def mecanum_1():
+    #speed = -gamepad1.left_stick_y
+    #turn = gamepad1.right_stick_x
+    #strafe = gamepad1.left_stick_x
+
+    #leftFront = speed + turn + strafe
+    #rightFront = speed - turn - strafe
+    #leftRear = speed + turn - strafe
+    #rightRear = speed - turn + strafe
+
+#Method 2 - better than method 1
+#theta should be 360
+#power is linear speed in direction of theta
+#turn is rotational speed - clockwise or counterclockwise
+def mecanum_2(theta, power, turn):
+    if (hoop_detection):
+        leftFront = 0
+        rightFront = 0
+        leftRear = 0
+        rightRear = 0
+    else:
+        sin = Math.sin(theta - Math.PI/4)
+        cos = Math.cos(theta - Math.PI/4)
+        max = Math.max(Math.abs(sin), Math.abs(cos))
+
+        leftFront = power * cos/max + turn
+        rightFront = power * sin/max - turn
+        leftRear = power * sin/max + turn
+        rightRear = power * cos/max - turn
+
+        if ((power + Math.abs(turn)) > 1):
+            leftFront /= power + turn
+            rightFront /= power + turn
+            leftRear /= power + turn
+            rightRear /= power + turn
+
+#Computer vision functionality
+def hoop_detection():
+    #Return true when LiDAR sees the hoop
+    return False
