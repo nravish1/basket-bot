78fc391ef545de96214337b47d126c9a7c345687
Abel Saj <abelsaj@unc.edu>
2025-04-25 17:01:47 -0400

    trial

--- vision/main.py
+++ vision/main.py
@@ -24,10 +24,10 @@ if __name__ == "__main__":
             detector.pid_error = error
             detector.pid_output = output
 
-            motor_speeds = motor_controller.simple_rotation(output, max_speed=255) # Simpler (better when starting out)
+            #motor_speeds = motor_controller.simple_rotation(output, max_speed=255) # Simpler (better when starting out)
             # motor_speeds = motor_controller.mecanum_drive(output, max_speed=255) # Anuj's function
 
-            motor_controller.send_to_motors(motor_speeds)
+            #motor_controller.send_to_motors(motor_speeds)
 
             print(f"Error: {error:.2f}, PID Output: {output:.2f}")
         else:
